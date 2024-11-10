799ff4c5eaea483836437dcfe10cd7ab13e1a0db
Sairam <pvsairam@hotmail.com>
2024-11-10 15:16:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ print_banner() {
     echo "                                           |___/"      
     echo
     echo "                Titan Edge Installer v1.0"
-    echo "                Author: Galkurta"
+    echo "                Follow https://t.me/Airdrop_OG/"
     echo -e "${NC}\n"
 }
 
